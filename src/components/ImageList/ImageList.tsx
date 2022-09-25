8429b924629be45b908fa7acc97a4e16c9d2ed07
Ismail Khaled <esmael449@stud.fci-cu.edu.eg>
2022-09-25 19:24:37 +0200

    finished Loader component

--- src/components/ImageList/ImageList.tsx
+++ src/components/ImageList/ImageList.tsx
@@ -2,23 +2,28 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import Image from '../Image/Image';
+import Loader from '../Loader/Loader';
 import './ImageList.css';
 
 const ImageList: React.FC = () => {
   const images = useSelector((state: RootState) => state.image.filteredImages);
   return (
     <div className="image-list">
-      {images.map((image) => {
-        return (
-          <Image
-            key={image.id}
-            id={image.id}
-            desc={image.desc}
-            url={image.url}
-            password={image.password}
-          />
-        );
-      })}
+      {images.length === 0 ? (
+        <Loader />
+      ) : (
+        images.map((image) => {
+          return (
+            <Image
+              key={image.id}
+              id={image.id}
+              desc={image.desc}
+              url={image.url}
+              password={image.password}
+            />
+          );
+        })
+      )}
     </div>
   );
 };
